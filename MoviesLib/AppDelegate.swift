0278262c991094e2e005ffbdc5fc7f1e2e0e9abf
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-09-22 23:09:38 -0300

    Adicionado novas constraint para portrait e landscape

--- MoviesLib/AppDelegate.swift
+++ MoviesLib/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window?.tintColor = UIColor(named: "Main")
+        
         // Override point for customization after application launch.
         return true
     }
