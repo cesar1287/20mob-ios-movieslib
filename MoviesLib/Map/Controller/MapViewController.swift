3598363d76eb83b1f41d2a32e11c198826c47aa0
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-09-26 17:09:53 -0300

    Adicionado mapa no app

---
+++ MoviesLib/Map/Controller/MapViewController.swift
@@ -0,0 +1,115 @@
+//
+//  MapViewController.swift
+//  MoviesLib
+//
+//  Created by Cesar Nascimento on 26/09/20.
+//  Copyright © 2020 Cesar Nascimento. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    lazy var locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        requestAuthorization()
+    }
+    
+    private func setupView() {
+        mapView.mapType = .standard
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        mapView.delegate = self
+        searchBar.delegate = self
+    }
+
+    func requestAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.lineWidth = 7.0
+        renderer.strokeColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        
+        return renderer
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        let camera = MKMapCamera()
+        camera.centerCoordinate = view.annotation!.coordinate
+        camera.pitch = 80
+        camera.altitude = 100
+        mapView.setCamera(camera, animated: true)
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem(
+            placemark: MKPlacemark(
+                coordinate: mapView.userLocation.coordinate
+            )
+        )
+        
+        request.destination = MKMapItem(
+            placemark: MKPlacemark(
+                coordinate: view.annotation!.coordinate
+            )
+        )
+        
+        let directions = MKDirections(request: request)
+        directions.calculate { (response, error) in
+            if error == nil {
+                guard let response = response,
+                      let route = response.routes.sorted(
+                        by: {$0.distance < $1.distance}
+                      ).first else {return}
+                
+                print(route.name)
+                print(route.distance)
+                print(route.expectedTravelTime)
+                
+                self.mapView.removeOverlays(self.mapView.overlays)
+                self.mapView.addOverlay(route.polyline, level: .aboveRoads)
+            }
+        }
+    }
+}
+
+extension MapViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        
+        let request = MKLocalSearch.Request()
+        request.region = mapView.region
+        request.naturalLanguageQuery = searchBar.text
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            if error == nil {
+                guard let response = response else {return}
+                self.mapView.removeAnnotations(self.mapView.annotations)
+                
+                for item in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = item.placemark.coordinate
+                    annotation.title = item.name
+                    annotation.subtitle = item.url?.absoluteString
+                    self.mapView.addAnnotation(annotation)
+                }
+                
+                self.mapView.showAnnotations(self.mapView.annotations, animated: true)
+            }
+        }
+    }
+}
